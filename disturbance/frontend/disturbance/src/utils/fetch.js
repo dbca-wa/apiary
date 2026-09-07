9b69974c2f888c2003c80e5adf17515ec75151b0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-07 11:32:12 +0800

    Broaden error handling to include non_field_errors property or data being an object before falling back to response status text.

--- disturbance/frontend/disturbance/src/utils/fetch.js
+++ disturbance/frontend/disturbance/src/utils/fetch.js
@@ -1,28 +1,41 @@
 export default {
-    fetchUrl: async function (url, options) {
-        return new Promise((resolve, reject) => {
-            let f = options === undefined ? fetch(url) : fetch(url, options);
-            f.then(
-                async (response) => {
-                    const data = await response.json();
-                    if (!response.ok) {
-                        let error =
-                            (data.constructor.name === 'Array' && data) ||
-                            (data && data.message) ||
-                            response.statusText;
-                        reject(error);
-                    }
-                    resolve(data);
-                },
-                (error) => {
-                    console.error(
-                        `There was an error fetching from ${url}`,
-                        error
-                    );
-                    error = new Error(error.NETWORK_ERROR);
-                    reject(error);
-                }
-            );
-        });
-    },
-}
+  fetchUrl: async function (url, options) {
+    return new Promise((resolve, reject) => {
+      let f = options === undefined ? fetch(url) : fetch(url, options);
+      f.then(
+        async (response) => {
+          let data;
+          try {
+            data = await response.json();
+          } catch {
+            try {
+              data = await response.text();
+            } catch {
+              data = null;
+            }
+          }
+
+          if (!response.ok) {
+            let error =
+              (data && Array.isArray(data) && data) ||
+              (data && data.message) ||
+              (data &&
+                (data.non_field_errors ||
+                  data.detail ||
+                  typeof data === "object"))
+                ? data
+                : response.statusText;
+
+            return reject(error);
+          }
+          resolve(data);
+        },
+        (error) => {
+          console.error(`There was an error fetching from ${url}`, error);
+          error = new Error(error.NETWORK_ERROR);
+          reject(error);
+        },
+      );
+    });
+  },
+};
