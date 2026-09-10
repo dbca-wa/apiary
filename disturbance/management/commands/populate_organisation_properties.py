74a57b2198e73b6d9af2e4ca45fb9d8abc25a042
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-10 09:33:18 +0800

    Clean up command and run it on all Organisations not just those without property cache values since ledger is the source of truth and the values may change over time

--- disturbance/management/commands/populate_organisation_properties.py
+++ disturbance/management/commands/populate_organisation_properties.py
@@ -1,22 +1,23 @@
-from django.core.management.base import BaseCommand
-from disturbance.components.organisations.models import Organisation
-from django.db.models import Q
-
 import logging
+
+from django.core.management import CommandError
+from django.core.management.base import BaseCommand
+
+from disturbance.components.organisations.models import Organisation
 
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
-        
-        logger.info("Checking for organisations that are missing Org name or ABN from property cache.")
+        try:
+            # Use all organisations as the name or abn could be changed in ledger
+            for org in Organisation.objects.all():
+                org.update_property_cache()
+        except Exception as exc:
+            logger.exception(
+                "Unexpected error occurred while running populate_organisation_properties management command"
+            )
 
-        orgs_without_properties = Organisation.objects.filter(Q(property_cache__name__isnull=True)|Q(property_cache__abn__isnull=True))
-
-        logger.info(f"{orgs_without_properties.count()} org records to be updated.")
-
-        for org in orgs_without_properties:
-            org.update_property_cache()
-
-        logger.info(f"{orgs_without_properties.count()} org records updated.")
+            # 2. Crash the CLI gracefully with a clean exit code
+            raise CommandError(f"Command failed: {exc}")
