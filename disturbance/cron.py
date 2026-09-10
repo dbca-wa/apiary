8eca649268a8a248228922360974ac5831785202
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-10 09:34:15 +0800

    Add CronJobPopulateOrganisationProperties to run once daily early in the morning to resync the local organisations property cache name and abn from ledger so they are always update to date

--- disturbance/cron.py
+++ disturbance/cron.py
@@ -3,6 +3,8 @@ import logging
 import django_cron
 from django.core import management
 
+from disturbance import settings
+
 log = logging.getLogger(__name__)
 
 
@@ -27,4 +29,18 @@ class CronJobCronTasks(django_cron.CronJobBase):
     def do(self) -> None:
         log.info("Cron tasks cron job triggered, running...")
         management.call_command("cron_tasks")
+        return "Job Completed Successfully"
+
+
+class CronJobPopulateOrganisationProperties(django_cron.CronJobBase):
+    """Sync the local Organisation name and abn from ledger once per day in case details are changed."""
+
+    schedule = django_cron.Schedule(
+        run_weekly_on_days=[0, 1, 2, 3, 4, 5, 6], run_at_times=settings.POPULATE_ORGANISATION_PROPERTIES_RUN_AT_TIMES
+    )
+    code = "apiary.populate_organisation_properties"
+
+    def do(self) -> None:
+        log.info("Populate Organisation Properties Cron Job triggered, running...")
+        management.call_command("populate_organisation_properties")
         return "Job Completed Successfully"
