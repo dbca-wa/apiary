8498b93598c44547723af6792aa57cf0a3f4bac8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-10 14:52:05 +0800

    Remove _log_user_email and references to it as it was an incomplete method that was not actually creating any logs. Can add an EmailUserLogEntry in future if the users need it however it was not being used in many placed anyway and I don't think there is a page on the front end that lists the comms logs for a specific user anyway.

--- disturbance/components/ap_payments/email.py
+++ disturbance/components/ap_payments/email.py
@@ -1,46 +1,48 @@
 import logging
 
-from django.core.mail import EmailMultiAlternatives, EmailMessage
-from django.utils.encoding import smart_bytes
-from django.conf import settings
 import requests
-from disturbance.components.emails.emails import TemplateEmailBase
-from disturbance.components.ap_payments.invoice_pdf import create_invoice_pdf_bytes
-from disturbance.context_processors import apiary_url
+from django.conf import settings
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.utils.encoding import smart_bytes
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+
+from disturbance.components.emails.emails import TemplateEmailBase
 
 logger = logging.getLogger(__name__)
 
-SYSTEM_NAME = settings.SYSTEM_NAME_SHORT + ' Automated Message'
+SYSTEM_NAME = settings.SYSTEM_NAME_SHORT + " Automated Message"
+
 
 def get_sender_user():
     sender = settings.DEFAULT_FROM_EMAIL
     try:
         sender_user = EmailUser.objects.get(email__icontains=sender)
     except:
-        EmailUser.objects.create(email=sender, password='')
+        EmailUser.objects.create(email=sender, password="")
         sender_user = EmailUser.objects.get(email__icontains=sender)
     return sender_user
 
+
 class ApplicationFeeInvoiceApiarySendNotificationEmail(TemplateEmailBase):
-    subject = 'Your application fee invoice.'
-    html_template = 'disturbance/emails/payments/apiary/send_application_fee_notification.html'
-    txt_template = 'disturbance/emails/payments/apiary/send_application_fee_notification.txt'
+    subject = "Your application fee invoice."
+    html_template = "disturbance/emails/payments/apiary/send_application_fee_notification.html"
+    txt_template = "disturbance/emails/payments/apiary/send_application_fee_notification.txt"
+
 
 def send_application_fee_invoice_apiary_email_notification(request, proposal, invoice, recipients, is_test=False):
     print("send_application_fee_invoice_apiary_email_notification")
     email = ApplicationFeeInvoiceApiarySendNotificationEmail()
 
     context = {
-        'lodgement_number': proposal.lodgement_number,
+        "lodgement_number": proposal.lodgement_number,
     }
 
-    filename = 'invoice#{}.pdf'.format(invoice.reference)
+    filename = f"invoice#{invoice.reference}.pdf"
 
     api_key = settings.LEDGER_API_KEY
-    url = settings.LEDGER_API_URL+'/ledgergw/invoice-pdf/'+api_key+'/' + invoice.reference
+    url = settings.LEDGER_API_URL + "/ledgergw/invoice-pdf/" + api_key + "/" + invoice.reference
     invoice_pdf = requests.get(url=url)
-    attachment = (filename, invoice_pdf, 'application/pdf')
+    attachment = (filename, invoice_pdf, "application/pdf")
 
     msg = email.send(recipients, attachments=[attachment], context=context)
     if is_test:
@@ -50,19 +52,25 @@ def send_application_fee_invoice_apiary_email_notification(request, proposal, in
     _log_proposal_email(msg, proposal, sender=sender)
     if proposal.applicant:
         _log_org_email(msg, proposal.applicant, proposal.submitter, sender=sender)
-    else:
-        _log_user_email(msg, proposal.submitter, proposal.submitter, sender=sender)
+
 
 def _log_proposal_email(email_message, proposal, sender=None):
     from disturbance.components.proposals.models import ProposalLogEntry
-    if isinstance(email_message, (EmailMultiAlternatives, EmailMessage,)):
+
+    if isinstance(
+        email_message,
+        (
+            EmailMultiAlternatives,
+            EmailMessage,
+        ),
+    ):
         text = email_message.body
         subject = email_message.subject
         fromm = smart_bytes(sender) if sender else smart_bytes(email_message.from_email)
         # the to email is normally a list
         if isinstance(email_message.to, list):
             print(email_message.to)
-            to = ','.join(email_message.to)
+            to = ",".join(email_message.to)
         else:
             to = smart_bytes(email_message.to)
         # we log the cc and bcc in the same cc field of the log entry as a ',' comma separated string
@@ -71,28 +79,28 @@ def _log_proposal_email(email_message, proposal, sender=None):
             all_ccs += list(email_message.cc)
         if email_message.bcc:
             all_ccs += list(email_message.bcc)
-        all_ccs = ','.join(all_ccs)
+        all_ccs = ",".join(all_ccs)
 
     else:
         text = smart_bytes(email_message)
-        subject = ''
+        subject = ""
         to = proposal.submitter.email
         fromm = smart_bytes(sender) if sender else SYSTEM_NAME
-        all_ccs = ''
+        all_ccs = ""
 
     customer = proposal.submitter
 
     staff = sender
 
     kwargs = {
-        'subject': subject,
-        'text': text,
-        'proposal': proposal,
-        'customer': customer,
-        'staff': staff,
-        'to': to,
-        'fromm': fromm,
-        'cc': all_ccs
+        "subject": subject,
+        "text": text,
+        "proposal": proposal,
+        "customer": customer,
+        "staff": staff,
+        "to": to,
+        "fromm": fromm,
+        "cc": all_ccs,
     }
 
     email_entry = ProposalLogEntry.objects.create(**kwargs)
@@ -100,15 +108,22 @@ def _log_proposal_email(email_message, proposal, sender=None):
     return email_entry
 
 
-def _log_org_email(email_message, organisation, customer ,sender=None):
+def _log_org_email(email_message, organisation, customer, sender=None):
     from disturbance.components.organisations.models import OrganisationLogEntry
-    if isinstance(email_message, (EmailMultiAlternatives, EmailMessage,)):
+
+    if isinstance(
+        email_message,
+        (
+            EmailMultiAlternatives,
+            EmailMessage,
+        ),
+    ):
         text = email_message.body
         subject = email_message.subject
         fromm = smart_bytes(sender) if sender else smart_bytes(email_message.from_email)
         # the to email is normally a list
         if isinstance(email_message.to, list):
-            to = ','.join(email_message.to)
+            to = ",".join(email_message.to)
         else:
             to = smart_bytes(email_message.to)
         # we log the cc and bcc in the same cc field of the log entry as a ',' comma separated string
@@ -117,70 +132,29 @@ def _log_org_email(email_message, organisation, customer ,sender=None):
             all_ccs += list(email_message.cc)
         if email_message.bcc:
             all_ccs += list(email_message.bcc)
-        all_ccs = ','.join(all_ccs)
+        all_ccs = ",".join(all_ccs)
     else:
         text = smart_bytes(email_message)
-        subject = ''
+        subject = ""
         to = customer
         fromm = smart_bytes(sender) if sender else SYSTEM_NAME
-        all_ccs = ''
+        all_ccs = ""
 
     customer = customer
 
     staff = sender
 
     kwargs = {
-        'subject': subject,
-        'text': text,
-        'organisation': organisation,
-        'customer': customer,
-        'staff': staff,
-        'to': to,
-        'fromm': fromm,
-        'cc': all_ccs
+        "subject": subject,
+        "text": text,
+        "organisation": organisation,
+        "customer": customer,
+        "staff": staff,
+        "to": to,
+        "fromm": fromm,
+        "cc": all_ccs,
     }
 
     email_entry = OrganisationLogEntry.objects.create(**kwargs)
 
     return email_entry
-
-
-def _log_user_email(email_message, emailuser, customer ,sender=None):
-    if isinstance(email_message, (EmailMultiAlternatives, EmailMessage,)):
-        text = email_message.body
-        subject = email_message.subject
-        fromm = smart_bytes(sender) if sender else smart_bytes(email_message.from_email)
-        # the to email is normally a list
-        if isinstance(email_message.to, list):
-            to = ','.join(email_message.to)
-        else:
-            to = smart_bytes(email_message.to)
-        # we log the cc and bcc in the same cc field of the log entry as a ',' comma separated string
-        all_ccs = []
-        if email_message.cc:
-            all_ccs += list(email_message.cc)
-        if email_message.bcc:
-            all_ccs += list(email_message.bcc)
-        all_ccs = ','.join(all_ccs)
-
-    else:
-        text = smart_bytes(email_message)
-        subject = ''
-        to = customer
-        fromm = smart_bytes(sender) if sender else SYSTEM_NAME
-        all_ccs = ''
-
-    customer = customer
-
-    staff = sender
-
-    kwargs = {
-        'subject': subject,
-        'text': text,
-        'emailuser': emailuser,
-        'customer': customer,
-        'staff': staff,
-        'to': to,
-        'fromm': fromm,
-        'cc': all_ccs
-    }
