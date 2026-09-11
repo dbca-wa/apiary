a2be932d2aaf06c1939781fffad615c09347dcd9
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-11 14:26:59 +0800

    Use unoserver to generate the pdf in memory to try to speed up pdf generation

--- disturbance/doctopdf.py
+++ disturbance/doctopdf.py
@@ -1,54 +1,54 @@
+import io
 import os
-from io import BytesIO
 
 from django.conf import settings
-from docxtpl import DocxTemplate, InlineImage, R
 from docx.shared import Mm
+from docxtpl import DocxTemplate, InlineImage, R
+from unoserver.client import UnoClient
+
 from disturbance.components.main.models import ApiaryGlobalSettings
 
 
 def create_apiary_licence_pdf_contents(approval, proposal, copied_to_permit, site_transfer_preview=None):
-
-    licence_template = ApiaryGlobalSettings.objects.get(key=ApiaryGlobalSettings.KEY_APIARY_LICENCE_TEMPLATE_FILE)
-
-    if licence_template._file:
-        path_to_template = licence_template._file.path
-    else:
-        # Use default template file
-        path_to_template = os.path.join(settings.BASE_DIR, 'disturbance', 'static', 'disturbance', 'apiary_authority_permit_template_v3.docx')
-
-    doc = DocxTemplate(path_to_template)
     from disturbance.components.approvals.serializers import ApprovalSerializerForLicenceDoc
 
-    path_to_image = os.path.join(settings.BASE_DIR, 'disturbance', 'static', 'disturbance', 'img', 'dbca-logo.jpg')
-    serializer_context = {
-            'approver_id': approval.approver_id,
-            'site_transfer_preview': site_transfer_preview,
-            }
-    context_obj = ApprovalSerializerForLicenceDoc(approval, context=serializer_context)
-    context = context_obj.data
-    context.update({
-        'dbca_logo': InlineImage(doc, image_descriptor=path_to_image, width=Mm(135), height=Mm(20)),
-        'page_break': R('\f'),
-    })
+    # 1. Fetch the docx template
+    licence_template = ApiaryGlobalSettings.objects.filter(
+        key=ApiaryGlobalSettings.KEY_APIARY_LICENCE_TEMPLATE_FILE
+    ).first()
 
+    if licence_template and licence_template._file:
+        path_to_template = licence_template._file.path
+    else:
+        path_to_template = os.path.join(
+            settings.BASE_DIR, "disturbance", "static", "disturbance", "apiary_authority_permit_template_v3.docx"
+        )
+
+    doc = DocxTemplate(path_to_template)
+    path_to_image = os.path.join(settings.BASE_DIR, "disturbance", "static", "disturbance", "img", "dbca-logo.jpg")
+
+    # 2. Serialize and build template context
+    serializer_context = {
+        "approver_id": approval.approver_id,
+        "site_transfer_preview": site_transfer_preview,
+    }
+    context = ApprovalSerializerForLicenceDoc(approval, context=serializer_context).data
+    context.update(
+        {
+            "dbca_logo": InlineImage(doc, image_descriptor=path_to_image, width=Mm(135), height=Mm(20)),
+            "page_break": R("\f"),
+        }
+    )
+
+    # 3. Render DOCX template in memory
     doc.render(context)
 
-    temp_directory = settings.BASE_DIR + "/tmp/"
-    try:
-        os.stat(temp_directory)
-    except:
-        os.mkdir(temp_directory)
+    doc_io = io.BytesIO()
+    doc.save(doc_io)
+    doc_bytes = doc_io.getvalue()
 
-    f_name = temp_directory + 'apiary_licence_' + str(approval.id)
-    new_doc_file = f_name + '.docx'
-    new_pdf_file = f_name + '.pdf'
-    doc.save(new_doc_file)
-    os.system("libreoffice --headless --convert-to pdf " + new_doc_file + " --outdir " + temp_directory)
+    # 4. Convert directly to PDF bytes via UnoClient (In-memory via socket)
+    client = UnoClient(server="127.0.0.1", port=2002)
+    pdf_bytes = client.convert(indata=doc_bytes, convert_to="pdf")
 
-    file_contents = None
-    with open(new_pdf_file, 'rb') as f:
-        file_contents = f.read()
-    os.remove(new_doc_file)
-    os.remove(new_pdf_file)
-    return file_contents
+    return pdf_bytes
