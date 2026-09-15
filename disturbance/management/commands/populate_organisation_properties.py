b8af53f0819362d7b2fdabf08daa05b458634938
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-09-15 13:25:57 +0800

    fix: Catch the validation error (which is raised when the get organisation request fails), log a warning and continue trying the rest of the organisations.

--- disturbance/management/commands/populate_organisation_properties.py
+++ disturbance/management/commands/populate_organisation_properties.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.core.exceptions import ValidationError
 from django.core.management import CommandError
 from django.core.management.base import BaseCommand
 
@@ -13,7 +14,12 @@ class Command(BaseCommand):
         try:
             # Use all organisations as the name or abn could be changed in ledger
             for org in Organisation.objects.all():
-                org.update_property_cache()
+                try:
+                    org.update_property_cache()
+                except ValidationError as exc:
+                    # Log it and move to the next organisation instead of crashing the cron job
+                    self.stdout.write(self.style.WARNING(f"Skipping Org ID {org.id}: {exc}"))
+                    continue
         except Exception as exc:
             logger.exception(
                 "Unexpected error occurred while running populate_organisation_properties management command"
